a33cc0c89755dba0d5b94ae022f83e25ddf0708d
Sadadev <Sadamichael5@gmail.com>
2024-10-04 13:26:13 +0100

    first commit

--- learn.js
+++ learn.js
@@ -1,1 +1,51 @@
-let namE= 'Sada';
+/*let namE= 'Sada';
+
+let  score= 78;
+
+score+=10;
+
+console.log (score);*/
+
+//ARRAYS
+
+/*const heroes = [];
+heroes[0] = 'superman';
+heroes[1] = 'batman';
+heroes[2] = 'spiderman';
+heroes [4] = 'captain america';*/
+
+//let avengers = ['captain america', 'iron man', 'spiderman', 'Thor', 'Hulk'];
+
+//delete avengers[2]; To delete an item from the array
+
+//assigning multiple values at the same time
+/*const [x,y] = [2,3];
+
+const statement=   'The values of x and y are'
+
+console.log(statement + ' ' + x + ' ' + 'and'+ ' ' + y);*/
+
+//console.log(avengers.length) to find the lenght of an array 
+
+//avengers.length= 7; to extend the length of an array 
+
+//avengers.pop(); to remove the last item of an array
+
+//avengers.shift(); this removes the first item of an array
+
+//avengers.push('black panther'); To add a new item to the end of the list 
+
+//avengers.unshift('black widow');  This adds a new item to the front of the array
+
+/*avengers = avengers.concat(['hawkeye','black widow']); to concactinate two arrays
+or
+avengers = [...avengers, ...['hawkeye','black widow'] ];*/
+
+//avengers.reverse(); To reverse the order of an array
+
+//avengers.sort(); To sort the order of an array  
+
+
+
+
+console.log(avengers);
